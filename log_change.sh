b09b540d44a29877811476d16840cb54ec46278d
at055612 <22818309+at055612@users.noreply.github.com>
2021-11-02 11:58:25 +0000

    Update scripts

--- log_change.sh
+++ log_change.sh
@@ -474,7 +474,7 @@ list_unreleased_changes() {
           -n1 \
           "${file}" \
       )"
-      list_output+="${BLUE}${filename}${NC}:\n${YELLOW}${change_entry_line}${NC}\n\n"
+      list_output+="${BLUE}${filename}${NC}:\n${NC}${change_entry_line}${NC}\n\n"
     fi
   done
 
